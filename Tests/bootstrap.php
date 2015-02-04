9f611eaaf7eab5095698cf6c1211a87e943dc006
Nacho Colomina Torregrosa <ignacio.colomina@gmail.com>
2015-02-04 21:48:42 +0100

    Update bootstrap.php

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,4 +1,8 @@
-require_once __DIR__ . '/../vendor/autoload.php';
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+}
 
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
-\Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver::registerAnnotationClasses(); 
+require $autoloadFile;
+
+require_once __DIR__ . '/../Annotation/Operation.php';
+require_once __DIR__ . '/../Annotation/Stateable.php';
